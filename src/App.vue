e108a00455599b1c1db2e6d21c498cd613cdd468
Dmitry <indycar111@gmail.com>
2019-09-09 17:57:38 +0300

    добавлено хэширование

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,46 @@
 <template>
   <div id="app">
-      <addUser v-bind:addUserFunc="addUserFunc"/>
+      <template v-if="user!== null">
+      <addUser v-bind:addUserFunc="addUserFunc" v-bind:user="user"/>
       <html_List_Users v-bind:userList="userList"  v-bind:deleteUserFunc="deleteUserFunc"/>
+      </template>
+
+      <template v-else>
+      <enter v-bind:checkUserFunc="checkUserFunc" v-bind:token="token"/>
+      </template>
   </div>
 </template>
+
 
 <script>
 import addUser from './components/addUser.vue'  //импортирую файл
 import html_List_Users from './components/html_List_Users.vue'  //импортирую файл
+import enter from './components/enter.vue'
 import axios from "axios"
 
 
 
 export default {
   name: 'app',
+
   data() {
     return {
       userList: [],
+      user:null,
+      token: null
     }
+
   },
   mounted() {
-     this.reload()
+     this.reload();
+      this.token=localStorage.getItem('jwttoken')
+
   },
 
   components: {
     html_List_Users,     //html_List_Users нужно сделать в 3-х местах APP
-    addUser
+    addUser,
+    enter
   },
     methods: {
         addUserFunc: function(name,login,password) {
@@ -44,14 +59,24 @@ export default {
        reload: function () {
            axios.get('http://localhost:3000/ajax/users.json/',{
                params:{
-                   t:Date.now()
+                   t:Date.now()  //вспомогательная функция, чтобы не кэшировалось
                }
-           })                                   //вспомогательная функция, чтобы не кэшировалось
+           })
            .then((response) => {
                //console.log(response.data);
                this.userList = response.data;   //метод для отрисовки табилцы через другой сервер
             })
-       }
+       },
+        checkUserFunc: function (login, password) {
+            axios.post(`http://localhost:3000/ajax/users.json/checkuser`, { login, password })
+                .then((response) => {
+                    // console.log(response.data.user_login)
+                    this.user = response.data.user_login;
+                    console.log(response.data.token);
+                    this.token=response.data.token;
+                    localStorage.setItem('jwttoken', response.data.token)
+                })
+        },
     }
 }
 </script>
---
+++ src/components/enter.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="example-2" class="text-center">
+        <p> Вход </p>
+        <p>  {{token}}  </p>
+        <input v-model="enterLoginUser" placeholder="Логин">
+        <input v-model="enterPasswordUser" placeholder="Пароль">
+        <button  v-on:click="checkUser">Войти</button>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "enter",
+        props:[ "checkUserFunc", "token"],
+        data() {
+            return {
+                enterLoginUser: null,
+                enterPasswordUser: null,
+            }
+        },
+        methods:{
+            checkUser: function () {
+                this.checkUserFunc(this.enterLoginUser,this.enterPasswordUser);
+                this.enterLoginUser="";
+                this.enterPasswordUser="";
+            },
+        }
+    }
+</script>
+
